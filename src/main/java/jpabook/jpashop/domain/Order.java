8aa2a37a4dee46cc7e391fe22036507cb245ee7f
lns13301 <rkdrbgks22@ks.ac.kr>
2020-09-06 12:13:49 +0900

    feat: Add Order field

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -1,4 +1,20 @@
 package jpabook.jpashop.domain;
 
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "ORDERS")
 public class Order {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private Long memberId;
+
+    private LocalDateTime orderDate;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;
 }
